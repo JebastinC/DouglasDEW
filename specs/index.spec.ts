50d2ec871cee90290f722e1af433ad3babc6b138
JebastinC <jebastinchithambaranathan@gmail.com>
2024-11-23 21:19:23 +0530

    updated

--- specs/index.spec.ts
+++ specs/index.spec.ts
@@ -4,20 +4,21 @@ import fs from 'fs';
 const data = JSON.parse(fs.readFileSync('data/test.json').toString());
 
 describe("Testing of https://www.douglas.de application", () => {
+    it("Launch app", async()=>{
+        await browser.maximizeWindow();
+        await browser.url('https://www.douglas.de/de');
+
+        expect(await browser.getTitle()).to.equal("Online-Parfümerie ✔️ Parfum & Kosmetik kaufen | DOUGLAS");
+        await browser.takeScreenshot();
+
+        let shadowRoot = await browser.$(`//div[@id='usercentrics-root']`);
+        let agreeButton = await shadowRoot.shadow$(`.eIFzaz`);
+        await agreeButton.click();
+    })
     for (const datum of data) {
         it(datum.name, async () => {
 
-            await browser.maximizeWindow();
-            await browser.url('https://www.douglas.de/de');
-
-            expect(await browser.getTitle()).to.equal("Online-Parfümerie ✔️ Parfum & Kosmetik kaufen | DOUGLAS");
-            await browser.takeScreenshot();
-
-            let shadowRoot = await browser.$(`//div[@id='usercentrics-root']`);
-            let agreeButton = await shadowRoot.shadow$(`.eIFzaz`);
-            await agreeButton.click();
-            await browser.$('//li[@class = "navigation-main-entry"]/a[normalize-space(text()) = "PARFUM"]').click();
-
+            await browser.$(`//li/a[normalize-space(text()) = "${datum.Navigation_Link}"]`).click();
             await sleep(5000);
             try {
                 await browser.$('//div[@class="navigation-backdrop__content"]').moveTo(100, 100);
@@ -31,11 +32,17 @@ describe("Testing of https://www.douglas.de application", () => {
                     await browser.$(`//div[contains(@class,"facet-wrapper")]//div[@class="facet__title" and normalize-space(text()) = "${filterParam}"]`).click();
                     await browser.$(`//div[contains(@class,"facet-wrapper")]//div[normalize-space(text()) = "${filterParam}" and @class="facet__title"]/parent::div[@class="facet facet--open"]//div[@class="facet-option__label"]/div[normalize-space(text()) = '${fd}']/ancestor::a[@role="checkbox"]`).click();
                     await sleep(1500);
+                    
                 }
             }
 
             await sleep(7000);
+            var count :string = await browser.$("//div[@class='product-overview__headline-wrapper']").getText();
+            console.log(count);
+            await sleep(1000);
+            await browser.takeScreenshot();
         });
+
     }
 });
 
@@ -45,5 +52,18 @@ function sleep(n: number) {
         setTimeout(() => {
             return resolve(true);
         }, n);
+    })
+}
+
+function clickOnElement(locator: string, timeoutInms: number) {
+    it("click on the object", async()=>{
+        try {
+            console.log(locator);
+            await browser.$(locator).waitForClickable({ timeout: $(timeoutInms) });
+            await browser.$(locator).click();
+        } catch (error) {
+            console.log("click step failed with error" + error)
+        }
+        
     })
 }
